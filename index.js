98952ff957738d6e53943b524cd7dfc39d8cbf81
pbaileydev <72949337+pbaileydev@users.noreply.github.com>
2020-11-16 22:09:31 -0500

    Added buttons
    added buttons to website

---
+++ index.js
@@ -0,0 +1,12 @@
+function gitRepo() {
+    window.location.href='https://github.com/pbaileydev?tab=repositories'
+}
+function googlePlay() {
+    window.location.href='https://play.google.com/store/apps/developer?id=Phillip+Bailey'
+}
+function solar(){
+    window.location.href='https://github.com/pbaileydev/SolarSystem'
+}
+function mathMania(){
+    window.location.href='https://github.com/pbaileydev/SolarSystem'
+}
